d9a52b9fd00e1c83aa10073e58fb7c1f99c22429
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2025-03-14 22:04:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('Welcome on Twitter!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}`)
+})
